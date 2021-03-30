a5e3a8788ec66d9cd0cf108c2e94cc574ed6cf4a
lbrugnagodamaia <l.brugnago@hotmail.com>
2021-03-30 20:29:19 -0300

    Create exercicio2.c

---
+++ exercicio2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int vetor[5], i, maiorElemento;
+	
+	for(i= 0; i < 5; i++){
+		printf("Digite um valor para a posição %i: ", i);
+		scanf("%i", %vetor[i]);
+		if(vetor[i] > maiorElemento)
+			maiorElemento = vetor[i];
+	}
+	
+	for(i = 0; i < 5; i++){
+		printf("%i \t", vetor[i]);
+	}
+	printf("\n Maior número do vetor : %i", maiorElemento)
+}
